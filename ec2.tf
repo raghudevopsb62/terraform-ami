d0e02f244295445464b809fe52dd99349739b733
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-22 08:21:15 +0530

    Update terraform-ami/

--- ec2.tf
+++ ec2.tf
@@ -16,7 +16,7 @@ resource "null_resource" "ansible-apply" {
       password = jsondecode(data.aws_secretsmanager_secret_version.latest.secret_string)["SSH_PASS"]
     }
     inline = [
-      "ansible-pull -U https://github.com/raghudevopsb62/ansible roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=ENV"
+      "ansible-pull -U https://github.com/raghudevopsb62/ansible roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=ENV -e strategy=immutable"
     ]
   }
 }
