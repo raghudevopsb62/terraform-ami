a24a3e35610ad6d775f1a584bff67c2089cd692f
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-22 08:17:41 +0530

    Update terraform-ami/

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ami" {
   ami                    = data.aws_ami.ami.id
-  aws_instance           = "t3.micro"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow-ssh.id]
   tags = {
     Name = "${var.COMPONENT}-ami"
