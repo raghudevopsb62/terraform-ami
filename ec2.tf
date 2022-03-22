ef282907741377d2f1c8e61bd3750627e661b8ea
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-22 08:16:45 +0530

    Update terraform-ami/

---
+++ ec2.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "ami" {
+  ami                    = data.aws_ami.ami.id
+  aws_instance           = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+}
+
+
--- main.tf
+++
@@ -1,5 +0,0 @@
-resource "null_resource" "sample" {
-  provisioner "local-exec" {
-    command = "echo ${var.COMPONENT}, ${var.APP_VERSION}"
-  }
-}
